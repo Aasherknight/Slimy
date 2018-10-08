b8af905ec027511ef23e29f45763dbc491438666
Aaron Gerber <aasherknight@gmail.com>
2018-10-08 12:33:22 -0400

    things

--- Actor.gd
+++ Actor.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Sprite
 
 # class member variables go here, for example:
 # var a = 2
@@ -9,6 +9,18 @@ func _ready():
 	# Initialization here
 	pass
 
+func moveLeft():
+	var newPos = Vector2(self.get_parent().get_rect().size.width/2,
+		self.get_parent().get_rect().size.height/2)
+	self.set_pos(newPos)
+	
+	self.set_rot(deg2rad(90))
+	pass
+	
+func _draw():
+	self.draw_rect(self.get_item_rect(),Color(0,0,1,0.2))
+	pass
+	
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
